3bb95666ba507f6a0e1a4411c583fb1dcf03873e
alexander iwsifidis <alexis_iwsifidis@hotmail.com>
2018-11-11 23:04:10 +0200

    add new section

--- js/script.js
+++ js/script.js
@@ -5,6 +5,21 @@ jQuery(".preload").fadeOut("slow");
 
 jQuery(".nav-button").click(function(){
     jQuery(".navmenu").toggleClass('navmenu--show');
+    jQuery(".video_toggle_button").hide();
+});
+jQuery(".video_switch_container").click(function(){
+const value=jQuery(".video_toggle_button").hasClass("btnSlide");
+
+if(value){
+jQuery(".video_toggle_button").addClass("btnSlide")
+jQuery("video").get(0).pause()
+}
+else{
+    jQuery(".video_toggle_button").addClass("btnSlide")
+    jQuery("video").get(0).pause()
+}
+
+
 });
 
 });
